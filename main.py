101d834891fa891acb15f41fb4cf8042b72b2dc8
anuroy15052005 <anuroy15052005@gmail.com>
2026-08-08 13:13:40 +0530

    Only need to train the model now

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ if str(src_path) not in sys.path:
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarizer.logging import logger
 
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} STARTED <<<<<<")
     data_ingestion =  DataIngestionTrainingPipeline()
@@ -27,7 +27,6 @@ except Exception as e:
 
 
 STAGE_NAME = "Data Validation stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} STARTED <<<<<<")
     data_validation =  DataValidationTrainingPipeline()
@@ -37,12 +36,24 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME = "Data Transformation stage"
 
+STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} STARTED <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} COMPLETED <<<<<<\n\n x=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f"**********************")
+    logger.info(f">>>>>> stage {STAGE_NAME} STARTED <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} COMPLETED <<<<<<\n\n x=========x")
 except Exception as e:
     logger.exception(e)
