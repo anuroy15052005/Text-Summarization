9dcd982faccaecc83360d51600921c6e8742a0b8
anuroy15052005 <anuroy15052005@gmail.com>
2026-09-04 11:24:17 +0530

    Streamlit version for free deployment

--- app.py
+++ app.py
@@ -1,42 +1,14 @@
 import os
 os.environ["NUMEXPR_MAX_THREADS"] = "16"
 
-from fastapi import FastAPI, Request
-import uvicorn
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from starlette.responses import RedirectResponse
-from fastapi.responses import Response
+import streamlit as st
 from textSummarizer.pipeline.prediction import PredictionPipeline
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+st.title("Dialogue Summarizer")
+text = st.text_area("Enter dialogue:")
 
-
-@app.get("/", tags=["authentication"])
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/train")
-async def training():
-    try:
-        os.system("python main.py")
-        return Response("Training successful !!")
-    except Exception as e:
-        return Response(f"Error Occurred! {e}")
-
-
-@app.post("/predict")
-async def predict_route(text: str):
-    try:
-        obj = PredictionPipeline()
-        summary = obj.predict(text)
-        return summary
-    except Exception as e:
-        raise e
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+if st.button("Summarize"):
+    obj = PredictionPipeline()
+    summary = obj.predict(text)
+    st.write("**Summary:**")
+    st.write(summary)
